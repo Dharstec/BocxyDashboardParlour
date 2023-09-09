9216522bcb81d4152b08ded2dbaeeac1bae682a9
Pavi87 <mugeshmuggi933@gmail.com>
2023-09-09 11:21:29 +0530

    Dashboard

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { CouponsModule } from './coupons/coupons.module';
+import { CustomersComponent } from './customers/customers.component';
+import { AddproductComponent } from './dashboard/inventory/addproduct/addproduct.component';
+import { OrderComponent } from './dashboard/inventory/order/order.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'analytic', pathMatch: 'full' },
+  { path: 'analytic', component: AnalyticsComponent },
+  { path: 'coupon', loadChildren: () => (import('./coupons/coupons.module')).then((m) => m.CouponsModule) },
+  { path: 'marketing', loadChildren: () => (import('./marketing/marketing.module')).then((m) => m.MarketingModule) },
+  { path: 'inventory', loadChildren: () => (import('./inventory/inventory.module')).then((m) => m.InventoryModule) },
+  { path: 'order', loadChildren: () => (import('./order/order.module')).then((m) => m.OrderModule) },
+  { path: 'customers', loadChildren: () => (import('./customers/customer.modules')).then((m) => m.CustomersModule) },
+  { path: 'productDetails', component: AddproductComponent, data: { title: 'ProductDetails' } },
+  { path: 'order', component: OrderComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/inventory/add-product/add-product.model.ts
@@ -0,0 +1,24 @@
+export class AddProduct{
+    _id:any;
+    productName:any;
+    discountPrice:any;
+    actualPrice:any;
+    category:any;
+    stock:any
+    stone:any
+    colour:any
+    style:any
+    personalised:any
+    latest:any
+    collections:any
+    viewedBy:any
+    noOfViews:any
+    noOfSales:any
+    productAge:any
+    referenceId:any 
+    barcode:any
+    imageArray:any;
+    videoArray:any;
+  gift: any;
+  description: any;
+}
