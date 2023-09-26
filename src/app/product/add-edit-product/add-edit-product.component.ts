8779026f2e2cdfcb27e8166166482b2a95e731a1
Pavi87 <mugeshmuggi933@gmail.com>
2023-09-26 22:08:28 +0530

    Product

--- src/app/product/add-edit-product/add-edit-product.component.ts
+++ src/app/product/add-edit-product/add-edit-product.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, UntypedFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router, ActivatedRoute } from '@angular/router';
 import { couponsList } from 'src/app/coupons/coupons.model';
+import { AddProduct } from 'src/app/inventory/add-product/add-product.model';
 import { ApiService } from 'src/app/services/api.service';
 import { SnackbarComponent } from 'src/app/shared-module/snackbar/snackbar.component';
 
@@ -13,129 +14,311 @@ import { SnackbarComponent } from 'src/app/shared-module/snackbar/snackbar.compo
 })
 export class AddEditProductComponent implements OnInit {
   form: FormGroup;
-  types: string[] = [
-    "Affiliate",
-    "In Store",
-  ];
+  editproduct: any;
+  productview = false;
+  changename: any;
+  changetheproductName: any
+  @ViewChild('fileInput') fileInput: ElementRef;
+  videoSelect = false;
+  mainImageSrc: string;
+  apiMainImageSrc: string;
+  images: Array<string> = [];
+  apiMainImages: Array<string> = [];
+  video: string;
+  apiVideoUrl: string;
   submitted = false;
+  noImage = "assets/no_found.jpeg"
+  types: string[] = [
+    "Male",
+    "Female",
+    "Neutral"
+  ];
+  category = [
+    "Haircare products",
+    "Skincare products",
+    "Nailcare products",
+    "Cosmetics",
+    "Makeup",
+    "Fragrance",
+    "Serums",
+    "Sunscreens"
+  ]
+  stone = [
+    'LOréal',
+    'Maybelline',
+    'MAC',
+    'Lakme',
+    'Wella',
+    'Toni & Guy',
+    'TRESemmé',
+    'Renee'
+  ]
+  color = [
+    "Liquid",
+    "Stick",
+    "Cream",
+    "Balm",
+    "Gel"  ]
+  style = [
+    "4 stars & above",
+    "3 stars & above",
+    "2 stars & above",
+    "1 star & above"
+    ]
+  collections = [
+    "Diwali", "New Year", "Mother's Day", "Christmas", "Raksha Bandhan", "Eid", "Holi", "Durga pooja", ""
+  ]
+  productId: string;
   edit = false;
-  couponId: string;
   view = false;
-  couponsDetails: any;
-  constructor(private api: ApiService, private fb: FormBuilder, private router: Router, private snackbar: MatSnackBar, private activeRoute: ActivatedRoute) {
+  uploadEnabled: boolean = true;
+  result: any;
+  productDetails: any;
+  isSave = false;
+
+  constructor(private router: Router, private formBuilder: UntypedFormBuilder, private api: ApiService, private snackbar: MatSnackBar, private activeRoute: ActivatedRoute) {
     this.activeRoute.paramMap.subscribe(params => {
-      this.couponId = params.get('id');
-      if (this.couponId && this.router.url.includes('edit')) {
+      this.productId = params.get('id');
+      if (this.productId && this.router.url.includes('edit')) {
         this.edit = true;
-        this.getCouponDetails();
+        this.getProductDetails();
       } else if (this.router.url.includes('view')) {
         this.view = true;
-        this.getCouponDetails();
+        this.getProductDetails();
       }
-    })
-  }
 
-  getCouponDetails() {
-    this.api.apiGetDetailsCall(this.couponId, 'Coupon/findCoupon').subscribe(data => {
-      this.couponsDetails=data.data;
-      this.form.patchValue(data.data);
-      if (this.router.url.includes('view')) {
-        this.form.disable();
-      }
     })
   }
 
   ngOnInit(): void {
-    this.form = this.fb.group({
-      createdFor: ['', Validators.required],
-      couponName: ['', Validators.required],
-      description: ['', Validators.required],
-      discountPercentage: ['', Validators.required],
-      type: ['', Validators.required],
-      totalQuantity: ['', Validators.required],
-      limit: ['', Validators.required],
-      validDateTill: ['', Validators.required],
-      // totalCommissions: ['', Validators.required],
-      // totalOrders: ['', Validators.required],
-      // totalSales: ['', Validators.required],
-      // remaining: ['', Validators.required]
-    })
+    this.initializeForm();
+    if (!this.productId) {
+      this.mainImageSrc = this.noImage;
+      this.generateRandomString();
+    } 
   }
 
-  discard(){
-    if(this.couponId){
-      this.form.patchValue(this.couponsDetails);
-    }else{
-      this.form.reset();
+  generateRandomString(): string {
+    const characters: string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    this.result = "";
+
+    for (let i: number = 0; i < 12; i++) {
+      const randomIndex: number = Math.floor(Math.random() * characters.length);
+      this.result += characters.charAt(randomIndex);
     }
-    this.router.navigate(['/store/list'])
+
+    return this.result;
   }
 
-  saveCoupons(): void {
-    if (this.form.invalid) {
-      this.submitted = true;
-      return
-    } else {
-      this.submitted = false;
-      const CouponsAdd = new couponsList()
-      CouponsAdd._id = this.couponId ? this.couponId : null;;
-      CouponsAdd.createdFor = this.form.get('createdFor').value;
-      CouponsAdd.couponName = this.form.get('couponName').value;
-      CouponsAdd.description = this.form.get('description').value;
-      CouponsAdd.type = this.form.get('type').value;
-      CouponsAdd.discountPercentage = this.form.get('discountPercentage').value;
-      CouponsAdd.totalQuantity = this.form.get('totalQuantity').value;
-      CouponsAdd.limit = this.form.get('limit').value;
-      CouponsAdd.validDateTill = this.form.get('validDateTill').value;
 
-      // couponName:req.body.couponName,
-      // totalQuantity:req.body.totalQuantity,
-      // availedQuantity: req.body.availedQuantity,
-      // createdFor:req.body.createdFor,
-      // totalOrders:req?.body?.totalOrders || 0,
-      // totalSales:req?.body?.totalSales || 0,
-      // totalCommissions:req?.body?.totalCommissions || 0,
-      // createdDate:req.body.createdDate,
-      // validDateTill:req.body.validDateTill,
-      // discountPercentage:req.body.discountPercentage,
-      // type:req.body.type,
-      // description:req.body.description,
-      // remaining:req.body.remaining
+  getProductDetails() {
+    this.api.apiGetDetailsCall(this.productId, 'Product/getOneProduct').subscribe(data => {
+      this.productDetails = data.data;
+      this.form.patchValue(data.data);
+      this.mainImageSrc = this.productDetails?.productImages[0];
+      this.images = this.productDetails?.productImages;
+      this.video = this.productDetails?.productVideos[0];
+      if (this.router.url.includes('view')) {
+        this.form.disable();
+      }
+    })
 
-      // CouponsAdd.totalCommissions = this.form.get('totalCommissions').value;
-      // CouponsAdd.totalOrders = this.form.get('totalOrders').value;
-      // CouponsAdd.totalSales = this.form.get('totalSales').value;
-      // CouponsAdd.availedQuantity = this.form.get('availedQuantity')?.value ? this.form.get('availedQuantity').value : '0';
-      // CouponsAdd.remaining = this.form.get('remaining')?.value;
-      if (this.couponId) {
-        this.api.apiPutCall(CouponsAdd, 'Coupon/updateCoupon').subscribe(data => {
-          if (data.message.includes('Successfully')) {
-            this.snackbar.openFromComponent(SnackbarComponent, {
-              data: data.message,
-            });
-            this.router.navigate(['/store/list'])
-          }
-        }, (error) => {
-          if (error) {
-            this.form.reset();
-          }
-        })
-      } else {
-        this.api.apiPostCall(CouponsAdd, 'Coupon/createCoupon').subscribe(data => {
-          if (data.message.includes('Created Successfully')) {
-            this.snackbar.openFromComponent(SnackbarComponent, {
-              data: data.message,
-            });
-            this.router.navigate(['/store/list'])
-          }
-        }, (error) => {
-          if (error) {
-            this.form.reset();
-          }
-        })
+  }
+  allFiles: any;
+  onFileChange(event: any) {
+    const files = event.target.files;
+    this.allFiles = event.target.files
+    if (files.length > 0) {
+      const file = files[0];
+      if (file) {
+        this.handleImageVideoUpload(file);
       }
     }
   }
+  handleImageVideoUpload(file: File) {
+    const reader = new FileReader();
+    // if (!this.uploadEnabled) {
+    //   return;
+    // }
+
+    // if (this.images.length >= 4 && !this.video) {
+    //   console.log("You can only upload four images and one video.");
+    //   this.uploadEnabled = false;
+    //   return;
+    // }
+
+    // if (this.images.length === 4 && this.video) {
+    //   this.uploadEnabled = false;
+    // }
+
+    if (this.allFiles && this.allFiles[0]) {
+      const numberOfFiles = this.allFiles.length;
+      for (let i = 0; i < numberOfFiles; i++) {
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          if (e.target.result.includes('image/')) {
+            this.images.push(e.target.result);
+            this.mainImageSrc = this.images[0];
+          } else {
+            // const videoElement = document.createElement('video');
+            // videoElement.preload = 'metadata';
+            // videoElement.onloadedmetadata = () => {
+            //   window.URL.revokeObjectURL(videoElement.src);
+            //   if (videoElement.duration < 5 || videoElement.duration > 30) {
+            //     this.snackbar.openFromComponent(SnackbarComponent, {
+            //       data:"Video duration should be between 10 and 30 seconds.",
+            //     });
+            //     return;
+            //   }
+            this.video = e.target.result;
+            //   this.videoSelect=true;
+            // };
+            // videoElement.src = URL.createObjectURL(file);
+          }
+
+        }
+        reader.readAsDataURL(this.allFiles[i]);
+      }
+    }
+  }
+
+  selectImage(image: string) {
+    this.mainImageSrc = image;
+    this.videoSelect = false;
+  }
+  selectVideo(video: string) {
+    this.videoSelect = true;
+  }
+
+  removeImage(index: number) {
+    this.images.splice(index, 1);
+    if (!this.images.length) {
+      this.mainImageSrc = this.noImage;
+    }
+  }
+
+  removeVideo() {
+    this.video = "";
+  }
+
+  clearFileInput() {
+    this.fileInput.nativeElement.value = '';
+  }
+
+  initializeForm() {
+    this.form = this.formBuilder.group({
+      productName: ['', Validators.required], //bc name 
+      discountPrice: ['', Validators.required],
+      actualPrice: ['', Validators.required],
+      description: ['', Validators.required],
+      stock: ['', Validators.required],
+      category: ['', Validators.required],
+      stone: ['', Validators.required],
+      colour: ['', Validators.required],
+      style: ['', Validators.required],
+      for: ['', Validators.required],
+      gift: [true],
+      personalised: [true],
+      latest: [true],
+      collections: ['', Validators.required],
+      viewedBy: [''],
+      noOfViews: [''],
+      noOfSales: [''],
+      productAge: [''],
+      referenceId: ['', Validators.required],
+    })
+  }
+
+  discard() {
+    if (this.productId) {
+      this.form.patchValue(this.productDetails);
+    } else {
+      this.form.reset();
+    }
+    this.router.navigate(['/inventory/list'])
+  }
+
+
+  save(): void {
+    this.form.setValidators(null);
+    this.form.updateValueAndValidity();
+    if (this.form.invalid && this.allFiles?.length !== 5) {
+      this.submitted = true;
+      // if(!this.allFiles.includes('video')){
+
+      // }
+      // this.snackbar.openFromComponent(SnackbarComponent, {
+      //   data: data.message,
+      // });
+
+      return
+    } else {
+      this.submitted = false;
+      this.isSave = true;
+      const formData = new FormData()
+      if(this.allFiles && this.allFiles.length){
+        for (let img of this.allFiles) {
+          formData.append('files', img)
+        }
+      }
+     
+      this.api.apiPostCall(formData, 'Product/createProductImages').subscribe(data => {
+        if (data.message.includes('Image Added Successfully')) {
+          const addProd = new AddProduct()
+          addProd._id = this.productId ? this.productId : null
+          addProd.productName = this.form.get('productName')?.value;
+          addProd.discountPrice = this.form.get('discountPrice')?.value;
+          addProd.actualPrice = this.form.get('actualPrice')?.value;
+          addProd.description = this.form.get('description')?.value;
+          addProd.category = this.form.get('category')?.value;
+          addProd.stock = this.form.get('stock')?.value;
+          addProd.stone = this.form.get('stone')?.value;
+          addProd.colour = this.form.get('colour')?.value;
+          addProd.style = this.form.get('style')?.value;
+          addProd.gift = this.form.get('gift')?.value;
+          addProd.personalised = this.form.get('personalised')?.value;
+          addProd.latest = this.form.get('latest')?.value;
+          addProd.collections = this.form.get('collections')?.value;
+          addProd.viewedBy = this.form.get('viewedBy')?.value;
+          addProd.noOfViews = this.form.get('noOfViews')?.value;
+          addProd.noOfSales = this.form.get('noOfSales')?.value;
+          addProd.productAge = this.form.get('productAge')?.value;
+          addProd.referenceId = this.form.get('referenceId')?.value;
+          addProd.barcode = this.productId ? this.productDetails.barcode : this.result;
+          addProd.imageArray = data.data.imageArray ? data.data.imageArray : [];
+          addProd.videoArray = data.data.videoArray ? data.data.videoArray : [];
+          if (this.productId) {
+            this.api.apiPutCall(addProd, 'Product/updateProduct').subscribe(data => {
+              if (data.message.includes('Successfully')) {
+                this.isSave = false;
+                this.snackbar.openFromComponent(SnackbarComponent, {
+                  data: data.message,
+                });
+                this.router.navigate(['/inventory/list'])
+              }
+            }, (error) => {
+              if (error) {
+                this.isSave = false;
+                // this.form.reset();
+              }
+            })
+          } else {
+            this.api.apiFormDataPostCall(addProd, 'Product/createProduct').subscribe(data => {
+              if (data.message.includes('Successfully')) {
+                this.snackbar.openFromComponent(SnackbarComponent, {
+                  data: data.message,
+                });
+                this.router.navigate(['/inventory/list'])
+              }
+            }, (error) => {
+              if (error) {
+                // this.form.reset();
+              }
+            })
+          }
+        }
+      })
+
+    }
+  }
 }
-
-
--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -5,7 +5,6 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { ConfirmDialogComponent } from 'src/app/shared-module/confirm-dialog/confirm-dialog.component';
-import { ExcelMapComponent } from 'src/app/shared-module/excel-map/excel-map.component';
 import { SnackbarComponent } from 'src/app/shared-module/snackbar/snackbar.component';
 
 @Component({
@@ -15,71 +14,57 @@ import { SnackbarComponent } from 'src/app/shared-module/snackbar/snackbar.compo
 })
 export class ProductListComponent implements OnInit {
   dataSource = new MatTableDataSource<any>([]);
-  columnsToDisplay = ['email','name', 'mobile', 'action'];
-  types: string[] = [
-    "Affiliate",
-    "In Store",
-  ];
-  status = [
-   {'status':"Active",'value':true} ,
-    {'status':"Inactive",'value':false}
-  ]
-  selectedValue: string;
-  selectedStatus:string;
-  noData=false;
-  couponsListData: any;
-  filteredData: any[];
+  columnsToDisplay = ['id', 'category', 'Formulation', 'name', 'in_stock', 'action'];
 
-  constructor(private api: ApiService, public dialog: MatDialog, private snackbar: MatSnackBar, private router: Router) { }
+  catagoryList = [
+    "Haircare products",
+    "Skincare products",
+    "Nailcare products",
+    "Cosmetics",
+    "Makeup",
+    "Fragrance",
+    "Serums",
+    "Sunscreens"];
+  colorList = [
+    "Liquid",
+    "Stick",
+    "Cream",
+    "Balm",
+    "Gel"  
+  ]
+  inStock = [
+    "Less than 10",
+    "Less than 50",
+    "More than 100",
+  ]
+  selectedValue: any;
+  selectedColourValue: any;
+  selectedStockValue: any
+  filteredData: any[];
+  inventoryList: any;
+  originalData: any[];
+  noData=false;
+
+  constructor(private api: ApiService,public dialog: MatDialog, private snackbar: MatSnackBar, private router: Router) { }
 
   ngOnInit(): void {
-    this.getCouponsList();
+    this.getInventoryList();
   }
 
-  getCouponsList(): void {
-    this.api.apiGetCall('Coupon/getCoupon').subscribe((data) => {
-      this.couponsListData=data.data;
+  edit(type,id) {
+    this.router.navigate(['/product/'+type, id]);
+  }
+
+  getInventoryList(): void {
+    this.api.apiGetCall('Product/getProduct').subscribe((data) => {
+      this.inventoryList = data.data;
       this.dataSource.data = data.data.sort((a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt));
       if(!data.data?.length){
         this.noData=true;
       }
     })
-  }
-  openExportDialog(): void {
-    const dialogRef = this.dialog.open(ExcelMapComponent, {
-      width: '500px',
-      data: { headers: Object.keys(this.dataSource.data[0]), dataSource: this.dataSource.data }
-    });
+  } 
   
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.generateExcel(result);
-      }
-    });
-  }
-  generateExcel(headers: string[]) {
-    const data = this.dataSource.data.map((row: any) => {
-      const newRow = {};
-      headers.forEach((header, i) => {
-        newRow[header] = row[Object.keys(row)[i]];
-      });
-      return newRow;
-    });
-    // const worksheet = XLSX.utils.json_to_sheet(data);
-    // const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-    // const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-    // const blob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-    // const url = URL.createObjectURL(blob);
-    // const a = document.createElement('a');
-    // a.href = url;
-    // a.download = 'data.xlsx';
-    // a.click();
-  }
-  
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
   delete(id: string): void {
     const dialog = this.dialog.open(ConfirmDialogComponent, {
       width: '250px',
@@ -90,42 +75,39 @@ export class ProductListComponent implements OnInit {
     dialog.afterClosed().subscribe(data => {
       if (data) {
 
-        this.api.apiDeleteCall(id, 'Coupon/deleteCoupon').subscribe(response => {
+        this.api.apiDeleteCall(id, 'Product/deleteProduct').subscribe(response => {
           if (response.message.includes('Successfully')) {
             this.snackbar.openFromComponent(SnackbarComponent, {
               data: response.message,
             });
-            this.getCouponsList();
+            this.getInventoryList();
           }
         })
       }
     })
   }
 
-  edit(type,id) {
-    this.router.navigate(['/coupon/'+type, id]);
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   applyTypeFilter() {
-    if(this.selectedStatus?.length || this.selectedValue?.length){
-      this.filteredData = this.dataSource.data.filter(item => {
-        // Check if the item's category is included in the selectedValue array
-        if (this.selectedValue?.length && !this.selectedValue?.includes(item.type[0])) {
-          return false;
-        }
-        
-        // Check if the item's colour is included in the selectedColourValue array
-        if (this.selectedStatus?.length && !this.selectedStatus?.includes(item.couponStatus[0])) {
-          return false;
-        }
-        // If the item passed both filters, return true
-        return true;
-      });
-    }else{
-      this.filteredData=[];
-      this.dataSource.data=this.couponsListData;
-    }
-    
-    }
+  if(this.selectedColourValue?.length || this.selectedValue?.length){
+    this.filteredData = this.dataSource.data.filter(item => {
+      // Check if the item's category is included in the selectedValue array
+      if (this.selectedValue?.length && !this.selectedValue?.includes(item.category[0])) {
+        return false;
+      }
+      // Check if the item's colour is included in the selectedColourValue array
+      if (this.selectedColourValue?.length && !this.selectedColourValue?.includes(item.colour[0])) {
+        return false;
+      }
+      // If the item passed both filters, return true
+      return true;
+    });
+  }else{
+    this.filteredData=[];
+    this.dataSource.data=this.inventoryList;
+  }
 }
-
+}
